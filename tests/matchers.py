954b36113f6285501c045944bea37100c203398d
David Keijser <keijser@gmail.com>
2013-10-09 10:49:02 +0200

    improve error message in asserts of mocks

--- tests/matchers.py
+++ tests/matchers.py
@@ -29,15 +29,32 @@ class called_with(BaseMatcher):
     def _matches(self, item):
         return item.call_args == self.args
 
+    def describe_call(self, call, desc):
+        args, kwargs = call
+        desc.append_value(args)
+        desc.append_text(', ')
+        desc.append_value(kwargs)
+
+    def describe_mismatch(self, item, mismatch_description):
+        mismatch_description.append_text('was called with ')
+        self.describe_call(item.call_args, mismatch_description)
+
     def describe_to(self, desc):
-        args, kwargs = self.args
-        desc.append_text('called with %r, %r' % (args, kwargs))
+        desc.append_text('mock called with ')
+        self.describe_call(self.args, desc)
 
 
 class called_once_with(called_with):
     def _matches(self, item):
         return item.call_count == 1 and called_with._matches(self, item)
 
+    def describe_mismatch(self, item, mismatch_description):
+        if item.call_count != 1:
+            mismatch_description.append_text(
+                'was called %s times' % item.call_count)
+        else:
+            called_with.describe_mismatch(self, item, mismatch_description)
+
     def describe_to(self, desc):
-        args, kwargs = self.args
-        desc.append_text('called once with %r, %r' % (args, kwargs))
+        desc.append_text('mock called once with ')
+        self.describe_call(self.args, desc)
