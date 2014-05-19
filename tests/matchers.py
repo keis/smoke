1096432fa604732d181e57f126022e9c035c65d2
David Keijser <keijser@gmail.com>
2014-05-19 18:41:12 +0200

    make mock hamcrest matcher pretty

--- tests/matchers.py
+++ tests/matchers.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 from hamcrest.core.base_matcher import BaseMatcher
+from hamcrest.core.helpers.wrap_matcher import wrap_matcher, is_matchable_type
 from hamcrest import assert_that, instance_of
 
 
@@ -10,8 +11,10 @@ class RaisesContext(object):
 @contextmanager
 def assert_raises(matcher=None, message=''):
     # Short hand for instance_of matcher
-    if isinstance(matcher, (type,)):
+    if is_matchable_type(matcher):
         matcher = instance_of(matcher)
+    else:
+        matcher = wrap_matcher(matcher)
 
     context = RaisesContext()
     try:
@@ -22,39 +25,39 @@ def assert_raises(matcher=None, message=''):
     assert_that(context.exception, matcher, message)
 
 
-class called_with(BaseMatcher):
-    def __init__(self, *args, **kwargs):
+class CalledWith(BaseMatcher):
+    def __init__(self, args, kwargs, count=None):
         self.args = (args, kwargs)
+        self.count = count
 
     def _matches(self, item):
+        if self.count is not None and item.call_count != self.count:
+            return False
+
         return item.call_args == self.args
 
     def describe_call(self, call, desc):
         args, kwargs = call
-        desc.append_value(args)
+        desc.append_description_of(args)
         desc.append_text(', ')
-        desc.append_value(kwargs)
+        desc.append_description_of(kwargs)
 
     def describe_mismatch(self, item, mismatch_description):
-        mismatch_description.append_text('was called with ')
-        self.describe_call(item.call_args, mismatch_description)
+        if self.count is not None and item.call_count != self.count:
+            mismatch_description.append_text(
+                'was called %s times' % item.call_count)
+        else:
+            mismatch_description.append_text('was called with ')
+            self.describe_call(item.call_args, mismatch_description)
 
     def describe_to(self, desc):
         desc.append_text('mock called with ')
         self.describe_call(self.args, desc)
 
 
-class called_once_with(called_with):
-    def _matches(self, item):
-        return item.call_count == 1 and called_with._matches(self, item)
+def called_with(*args, **kwargs):
+    return CalledWith(args, kwargs)
 
-    def describe_mismatch(self, item, mismatch_description):
-        if item.call_count != 1:
-            mismatch_description.append_text(
-                'was called %s times' % item.call_count)
-        else:
-            called_with.describe_mismatch(self, item, mismatch_description)
 
-    def describe_to(self, desc):
-        desc.append_text('mock called once with ')
-        self.describe_call(self.args, desc)
+def called_once_with(*args, **kwargs):
+    return CalledWith(args, kwargs, count=1)
